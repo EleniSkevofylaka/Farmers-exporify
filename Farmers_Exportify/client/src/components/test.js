aab5a3f2f764bd0fef1822f1f92014724d5b16f1
EleniSkevofylaka <helenskevo02@gmail.com>
2025-01-31 18:06:28 +0300

    Initialize backend setup with dependencies and component structure

---
+++ Farmers_Exportify/client/src/components/test.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+        function test() { 
+            const name= 'chris';
+            return( 
+               <div>
+                <h1> Hello, {name}</h1>
+               </div>
+            );
+
+
+        }
+        export default test;
