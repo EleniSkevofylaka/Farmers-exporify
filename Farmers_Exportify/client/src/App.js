27eef3a5e4347f51beca4c40d07d7d469446d33f
EleniSkevofylaka <helenskevo02@gmail.com>
2025-01-31 22:30:35 +0300

    Structured App.js with routing

--- Farmers_Exportify/client/src/App.js
+++ Farmers_Exportify/client/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import Test from './components/test';
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './components/HomePage';
+import ProductsPage from './components/ProductsPage';
+import ContactPage from './components/ContactPage';
+
+const App = () => {
   return (
-    <>
-    <Test/>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
